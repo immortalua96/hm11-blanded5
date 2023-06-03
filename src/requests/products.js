0510a56f7db11a15a8d250dbbbffe23c37232568
OksaGurzhyi <gurzhiy.oksa@gmail.com>
2023-06-03 15:30:50 +0200

    task2

--- src/requests/products.js
+++ src/requests/products.js
@@ -1,13 +1,20 @@
 import { instance } from "../services/api";
 export async function getProducts() {
   try {
-    const response = await instance.get('/products')
-    const products = response.data.products
-  return products
+    const response = await instance.get("/products");
+    const products = response.data.products;
+    return products;
   } catch (error) {
     console.log(error);
   }
-  
 }
 
+export async function getOneProductById(id) {
+  try {
+    const response = await instance.get(`/products/${id}`);
 
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
