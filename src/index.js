0ff6c04a97c7f7b528bd44d1ba062d7b02653349
immortalua96 <romanshvagla@icloud.com>
2023-06-03 14:25:58 +0200

    task-1

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import './styles/normalize.css';
 import './styles/index.css';
+import { getProducts } from './requests/products'
+import { refs } from './refs';
+import { createProductsMarkup } from './services/markupService';
+async function renderProducts() {
+  try {
+    const response = await getProducts()
+    const markup = createProductsMarkup(response)
+    refs.listProducts.insertAdjacentHTML('beforeend',markup)
+  } catch (error) {
+    console.log(error);
+  }
+}
+renderProducts();
